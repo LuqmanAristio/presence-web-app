64dcf2c01a2d7b357fd2c172e90caa4f1043adb6
aristio170802@gmail.com <aristio170802@gmail.com>
2022-11-21 20:04:18 +0800

    Adding Login Page

---
+++ src/component/Login.js
@@ -0,0 +1,27 @@
+import '../style/App.css'
+
+const Login = () =>{
+    return(
+        <div className="loginContainer">
+            <div className="loginBox">
+                <h1>Welcome Back</h1>
+                <p>Enter your credential username and password</p>
+
+                <div className="inputField">
+                    <i class="fa-solid fa-envelope"></i>
+                    <input type="text" placeholder="Enter your ID" className="usernameField"/>
+                </div>
+                <div className="inputField">
+                    <i class="fa-solid fa-lock"></i>
+                    <input type="password" placeholder="Enter your password" className="passwordField"/>
+                </div>
+
+                <button className="loginButton">Sign In</button>
+            </div> 
+
+            <p className="forgotText">Forgot your password?<span> Contact Us</span></p>
+        </div>
+    )
+}
+
+export default Login;

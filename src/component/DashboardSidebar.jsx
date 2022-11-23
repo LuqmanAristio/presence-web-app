8c696d995d61d3486df8fc0f86aa027fd437b1fd
aristio170802@gmail.com <aristio170802@gmail.com>
2022-11-23 19:24:38 +0800

    Update dynamic calendar

--- src/component/DashboardSidebar.jsx
+++ src/component/DashboardSidebar.jsx
@@ -3,9 +3,32 @@ import styles from "../style/DashboardSidebar.module.css"
 export const DashboardSidebar = () => {
 
     let today = new Date();
-
     let month = today.toLocaleString('default', { month: 'long' });
     let year = today.getFullYear();
+    let monthNum = today.getMonth();
+    let dateNow = today.getDate();
+
+    let dateMonth = [];
+    let dateLast = [];
+    let dateNext = [];
+    let lastDate = new Date(year, monthNum + 1, 0).getDate();
+    let firstDay = new Date(year, monthNum, 1).getDay();
+    let datePrevMonth = new Date(year, monthNum, 0).getDate();
+    let lastDayMonth = new Date(year, monthNum, lastDate).getDay();
+
+    for(let i=firstDay; i>0; i--){
+        let realDate = datePrevMonth - i + 1;
+        dateLast.push(realDate); 
+    }
+        
+    for(let i=1; i<=lastDate; i++){
+        dateMonth.push(i); 
+    }
+
+    for(let i=lastDayMonth; i<6; i++){
+        let realDate = i - lastDayMonth + 1;
+        dateNext.push(realDate);
+    }
 
     return(
         <div className={styles.dashboardSidebar}>
@@ -15,45 +38,35 @@ export const DashboardSidebar = () => {
 
                 <div className={styles.dateInfo}>
                     <ul className={styles.dayName}>
+                        <li>Sun</li>
                         <li>Mon</li>
                         <li>Tue</li>
                         <li>Wed</li>
                         <li>Thu</li>
                         <li>Fri</li>
                         <li>Sat</li>
-                        <li>Sun</li>
                     </ul>
                     <ul className={styles.dateNum}>
-                        <li>1</li>
-                        <li>2</li>
-                        <li>3</li>
-                        <li>4</li>
-                        <li>5</li>
-                        <li>6</li>
-                        <li>7</li>
-                        <li>8</li>
-                        <li>9</li>
-                        <li>10</li>
-                        <li>11</li>
-                        <li>12</li>
-                        <li>13</li>
-                        <li>14</li>
-                        <li>15</li>
-                        <li>16</li>
-                        <li>17</li>
-                        <li>18</li>
-                        <li>19</li>
-                        <li>20</li>
-                        <li>21</li>
-                        <li>22</li>
-                        <li>23</li>
-                        <li>24</li>
-                        <li>25</li>
-                        <li>26</li>
-                        <li>27</li>
-                        <li>28</li>
-                        <li>29</li>
-                        <li>30</li>
+                        {
+                            dateLast.map((dateLast) => <li className={styles.inactiveDate}>{dateLast}</li>)
+                        }
+                        {
+                            dateMonth.map((dateMonth) => {
+
+                                if(dateNow === dateMonth){
+                                    return <li className={styles.activeDateToday}>{dateMonth}</li>
+                                }
+                                else{
+                                    return <li className={styles.activeDate}>{dateMonth}</li>
+                                }
+
+                                
+                                
+                            })
+                        }
+                        {
+                            dateNext.map((dateNext) => <li className={styles.inactiveDate}>{dateNext}</li>)
+                        }
                     </ul>
                 </div>
             </div>
